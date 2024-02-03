2197f3d81a49aa4a088a829fe1498e5eec65f313
Amira Sayed Mohaemd <99423625+AmiraSayedMohamed@users.noreply.github.com>
2024-02-03 13:49:41 +0200

    Create ReadVideo.py
    how to read video using open-cv python

---
+++ ReadVideo.py
@@ -0,0 +1,15 @@
+import cv2
+
+video = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video.read()
+    cv2.imshow("Video", frame)
+
+
+    k = cv2.waitKey(1)
+    if k == ord("q"):
+        break
+
+video.release()
+cv2.destroyAllWindows()
