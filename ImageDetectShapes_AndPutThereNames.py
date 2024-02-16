8a645a8a08329928357acf007472f2ec68437226
Amira Sayed Mohaemd <99423625+AmiraSayedMohamed@users.noreply.github.com>
2024-02-16 07:06:04 +0200

    Update ImageDetectShapes_AndPutThereNames.py

--- ImageDetectShapes_AndPutThereNames.py
+++ ImageDetectShapes_AndPutThereNames.py
@@ -9,10 +9,11 @@ _, thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
 contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
 for contour in contours:
-    approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
-    cv2.drawContours(img, [approx], -1, (255, 0, 255), 5)
+    approx = cv2.approxPolyDP(contour, 0.02* cv2.arcLength(contour, True), True)
+    cv2.drawContours(img, [approx], 0, (255, 0, 255), 5)
     x = approx.ravel()[0]
-    y = approx.ravel()[1]
+    y = approx.ravel()[1] - 10
+
     if len(approx) == 3:
         cv2.putText(img, "Triangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
     elif len(approx) == 4 :
